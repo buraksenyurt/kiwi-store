b537c80bb53e5b734e808ae81b4dbdf45ee584d3
buraksenyurt <burakselimsenyurt@gmail.com>
2025-05-30 16:02:10 +0300

    Added CORS support to the API

--- kiwi-store-metric-api/src/main.rs
+++ kiwi-store-metric-api/src/main.rs
@@ -1,16 +1,22 @@
 use std::{env, net::SocketAddr};
 
-use axum::{Router, routing::get};
+use axum::{Router, http::Method, routing::get};
 use data::get_metrics;
 use dotenvy::dotenv;
 use log::info;
 use sqlx::postgres::PgPoolOptions;
+use tower_http::cors::{Any, CorsLayer};
 
 mod data;
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     env_logger::init();
+
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods([Method::GET])
+        .allow_headers(Any);
 
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
 
@@ -20,13 +26,15 @@ async fn main() {
         .await
         .expect("Failed to connect to database");
 
-    let app = Router::new().route(
-        "/metrics",
-        get({
-            let pool = pool.clone();
-            move || get_metrics(pool)
-        }),
-    );
+    let app = Router::new()
+        .route(
+            "/metrics",
+            get({
+                let pool = pool.clone();
+                move || get_metrics(pool)
+            }),
+        )
+        .layer(cors);
 
     let address = SocketAddr::from(([127, 0, 0, 1], 5556));
     info!("API running at http://{}/metrics", address);
