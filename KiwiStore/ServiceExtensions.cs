33b93e76a83f282c1f12dedc6faeb30671fb3c31
buraksenyurt <burakselimsenyurt@gmail.com>
2025-06-02 20:02:25 +0300

    Fixed config section bind error.

--- KiwiStore/ServiceExtensions.cs
+++ KiwiStore/ServiceExtensions.cs
@@ -16,17 +16,13 @@ public static class ServiceExtensions
 
     public static IServiceCollection AddKiwiStoreClient(this IServiceCollection services, IConfiguration configuration)
     {
-        if (configuration.GetSection("KiwiStore") is not KiwiStoreSection section)
-        {
-            throw new InvalidOperationException("KiwiStore configuration section is missing.");
-        }
-
-        var host = section.Host ?? "127.0.0.1";
-        var port = section.Port;
-        var endpoint = new KiwiStoreEndpoint(host, port);
-
+        var configSection = configuration.GetSection("KiwiStore");
+        var section = new KiwiStoreSection();
+        configSection.Bind(section);
+        var endpoint = new KiwiStoreEndpoint(section.Host, section.Port);
         services.AddSingleton(endpoint);
         services.AddSingleton<IKiwiStoreClient, KiwiStoreClient>();
         return services;
     }
+
 }
