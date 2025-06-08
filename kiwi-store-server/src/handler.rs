287ddff81f8151d06c7d3b1f5684044127823b56
buraksenyurt <burakselimsenyurt@gmail.com>
2025-06-08 07:49:28 +0300

    Removed unnecessary loop

--- kiwi-store-server/src/handler.rs
+++ kiwi-store-server/src/handler.rs
@@ -15,65 +15,62 @@ use tokio::net::TcpStream;
 /// * `data_store` - The data store to keep the key-value pairs.
 pub async fn handle_request(mut stream: TcpStream, data_store: DataStore) {
     let mut buffer = [0; 1024];
-    loop {
-        let size = match stream.read(&mut buffer).await {
-            Ok(0) => break,
-            Ok(n) => n,
-            Err(e) => {
-                error!("{}", e);
-                break;
-            }
-        };
-
-        info!("Read {}(bytes)", size);
-
-        let request = String::from_utf8_lossy(&buffer[..size]);
-        let cmd = Command::parse(&request);
-
-        let response = match cmd {
-            Command::Ping => {
-                info!("PING");
-                "PONG\n".to_string()
-            }
-            Command::Set { key, value } => {
-                data_store.set(&key, &value).await;
-                "OK\n".to_string()
-            }
-            Command::Get { key } => {
-                info!("GET {}", key);
-                data_store
-                    .get(&key)
-                    .await
-                    .unwrap_or_else(|| "NOT FOUND\n".to_string())
-            }
-            Command::Remove { key } => {
-                info!("REMOVE {}", key);
-                if data_store.remove(&key).await {
-                    "OK\n".to_string()
-                } else {
-                    warn!("Key '{}' not found for removal", key);
-                    "NOT FOUND\n".to_string()
-                }
-            }
-            Command::List => {
-                let result = if data_store.is_empty().await {
-                    warn!("Data store is empty");
-                    "EMPTY STORE\n".to_string()
-                } else {
-                    data_store.keys().await.join("\n").to_string()
-                };
-                result
-            }
-            Command::Stats => {
-                let stats = data_store.stats().await;
-                format!("STATS: {}\n", stats)
-            }
-            Command::Invalid(cmd) => format!("ERROR: Unknown command '{}'\n", cmd),
-        };
-
-        if let Err(e) = stream.write_all(response.as_bytes()).await {
+    let size = match stream.read(&mut buffer).await {
+        Ok(0) => return,
+        Ok(n) => n,
+        Err(e) => {
             error!("{}", e);
-            break;
+            return;
         }
+    };
+
+    info!("Read {}(bytes)", size);
+
+    let request = String::from_utf8_lossy(&buffer[..size]);
+    let cmd = Command::parse(&request);
+
+    let response = match cmd {
+        Command::Ping => {
+            info!("PING");
+            "PONG\n".to_string()
+        }
+        Command::Set { key, value } => {
+            data_store.set(&key, &value).await;
+            "OK\n".to_string()
+        }
+        Command::Get { key } => {
+            info!("GET {}", key);
+            data_store
+                .get(&key)
+                .await
+                .unwrap_or_else(|| "NOT FOUND\n".to_string())
+        }
+        Command::Remove { key } => {
+            info!("REMOVE {}", key);
+            if data_store.remove(&key).await {
+                "OK\n".to_string()
+            } else {
+                warn!("Key '{}' not found for removal", key);
+                "NOT FOUND\n".to_string()
+            }
+        }
+        Command::List => {
+            let result = if data_store.is_empty().await {
+                warn!("Data store is empty");
+                "EMPTY STORE\n".to_string()
+            } else {
+                data_store.keys().await.join("\n").to_string()
+            };
+            result
+        }
+        Command::Stats => {
+            let stats = data_store.stats().await;
+            format!("STATS: {}\n", stats)
+        }
+        Command::Invalid(cmd) => format!("ERROR: Unknown command '{}'\n", cmd),
+    };
+
+    if let Err(e) = stream.write_all(response.as_bytes()).await {
+        error!("{}", e);
     }
 }
